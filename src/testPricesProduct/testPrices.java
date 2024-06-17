815b1cfe55abc66887fc0bd943cab2fe8a0309e6
Bader Abu sa'aleek <bader.sleman2000@gmail.com>
2024-06-17 05:18:48 +0300

    first Update Code

---
+++ src/testPricesProduct/testPrices.java
@@ -0,0 +1,47 @@
+package testPricesProduct;
+import java.util.List;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class testPrices {
+	WebDriver myD = new ChromeDriver();  
+	String user = "standard_user"; 
+	String password = "secret_sauce"; 
+	String website = "https://www.saucedemo.com/"; 
+	String login = "//input[@type='submit']";
+	
+	@Test(priority = 1 )
+	public void setup() {
+		myD.get(website);
+		WebElement textUser =  myD.findElement(By.xpath("//input[@id='user-name']"));
+		WebElement textpassword =  myD.findElement(By.xpath("//input[@id='password']"));
+		WebElement btnLogin = myD.findElement(By.xpath(login));
+		textUser.sendKeys(user);
+		textpassword.sendKeys(password);
+	btnLogin.click();	
+		
+	}
+	
+	@Test(priority = 2 )
+	public void addThePricesUpper$15() {
+		
+		List<WebElement> Prices = myD.findElements(By.xpath("//div[@data-test='inventory-item-price']")); 
+		List<WebElement> btnCard = myD.findElements(By.className("btn_primary"));
+ 
+		for(int i= 0 ;i<Prices.size() ;i++ ){
+			
+        	 double val = Double.parseDouble(Prices.get(i).getText().replace("$", ""));
+			
+			if(val < 15 ){btnCard.get(i).click();}
+	
+			
+		}
+		
+		
+	}
+	
+	
+}
